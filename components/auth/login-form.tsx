5e27161c5b9278d98a14682ff9285ff4a617fa67
Jhon <eduardcamayo13@gmail.com>
2024-06-23 21:06:52 -0500

    Actualizando componente de login-form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,83 +1,107 @@
 "use client";
 import * as z from "zod";
+import {  useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import CardWrapper from "./card-wrappers";
 import {
-  Form,
-  FormField,
-  FormControl,
-  FormItem,
-  FormLabel,
-  FormMessage,
+    Form,
+    FormField,
+    FormControl,
+    FormItem,
+    FormLabel,
+    FormMessage,
 } from "../ui/form";
 import { LoginSchema } from "@/schemas";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import FormError from "../form-error";
+import FormSuccess from "../form-success";
+import { login } from "@/actions/login";
 
 const LoginForm = () => {
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
+    const [error, setError] = useState<string | undefined>("")
+    const [success, setSuccess] = useState<string | undefined>("")
+    const [isPending, startTransition] = useTransition();
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
-    console.log(values);
-  };
+    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+        setError("")
+        setSuccess("")
+        startTransition(() => {
+            login(values)
+            .then((data) => {
+                setError(data.error)
+                setSuccess(data.success)
+            })
+        });
+    };
 
-  return (
-    <CardWrapper
-      headerLabel="Welcome back"
-      backButtonHrfe="/auth/register"
-      showSocial
-      backButtonLabel="Don't have an account?"
-    >
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <div className="space-y-4">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      placeholder="jhon@gmail.com"
-                      type="email"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input {...field} placeholder="********" type="password" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <FormError message="Something went wrong!"/>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-        </form>
-      </Form>
-    </CardWrapper>
-  );
+    return (
+        <CardWrapper
+            headerLabel="Welcome back"
+            backButtonHrfe="/auth/register"
+            showSocial
+            backButtonLabel="Don't have an account?"
+        >
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-6"
+                >
+                    <div className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            disabled={isPending}
+                                            placeholder="jhon@gmail.com"
+                                            type="email"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            disabled={isPending}
+                                            placeholder="********"
+                                            type="password"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <FormError message={error} />
+                    <FormSuccess message={success} />
+                    <Button disabled={isPending} type="submit" className="w-full">
+                        Login
+                    </Button>
+                </form>
+            </Form>
+        </CardWrapper>
+    );
 };
 
 export default LoginForm;
