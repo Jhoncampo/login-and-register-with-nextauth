5bc397adeab3a8f1b13cbebbcda748f38a1278b6
Jhon <eduardcamayo13@gmail.com>
2024-06-23 21:04:55 -0500

    Agregando Schema del registro

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,3 +9,11 @@ export const LoginSchema = z.object({
     })
 })
 
+export const RegisterSchema = z.object({
+    email: z.string().email({message: "Email is required"}),
+    password: z.string().min(6,{message: "Minimum 6 caracters required"}),
+    confirmPassword: z.string().min(6, {message: "Minimum 6 caracters required"}),
+    name: z.string().min(1,{message: "Name is required"})
+
+})
+
