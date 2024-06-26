f5b722d6e3f3f6af697071f7d9cf4335b8cad9aa
Jhon <eduardcamayo13@gmail.com>
2024-06-26 13:26:18 -0500

    Agregando auth de google y github

--- auth.config.ts
+++ auth.config.ts
@@ -3,9 +3,19 @@ import type { NextAuthConfig } from "next-auth";
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 import bcrypt from "bcryptjs";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export default {
     providers: [
+        GitHub({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
+        }),
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
         credentials({
             async authorize(credentials) {
                 const validatedFields = LoginSchema.safeParse(credentials);
