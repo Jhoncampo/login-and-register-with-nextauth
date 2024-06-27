b67d1382c2d9333d197c2964c119452aab2c2f43
Jhon <eduardcamayo13@gmail.com>
2024-06-27 09:27:12 -0500

    Agregando páginas de login y error y creando verificación de email

--- auth.ts
+++ auth.ts
@@ -5,6 +5,18 @@ import authConfig from "@/auth.config";
 import { getUserById } from "./data/user";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    pages: {
+        signIn: "/auth/login",
+        error: "/auth/error",
+    },
+    events: {
+        async linkAccount({ user }) {
+            await db.user.update({
+                where: { id: user.id },
+                data: { emailVerified: new Date() },
+            });
+        },
+    },
     callbacks: {
         // async signIn({user}){
         //     console.log(user)
@@ -12,7 +24,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         //     if (!existingUser || !existingUser.emailVerified) {
         //         return false
         //     }
-
 
         //     return true
         // },
