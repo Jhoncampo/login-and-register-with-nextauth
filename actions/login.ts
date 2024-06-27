49bbb3640cb2de66c29669cec210ca04b9cb2acc
Jhon <eduardcamayo13@gmail.com>
2024-06-27 14:32:18 -0500

    Agregando envio de verficación de email en login y registro

--- actions/login.ts
+++ actions/login.ts
@@ -4,6 +4,10 @@ import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
+import { getUserByEmail } from "@/data/user";
+import { error } from "console";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
@@ -11,6 +15,23 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
         return { error: "Invalid fields" };
     }
     const { email, password } = validatedFields.data;
+
+    const existingUser = await getUserByEmail(email);
+    if (!existingUser || !existingUser.email || !existingUser.password) {
+        return { error: "Email does not exist!" };
+    }
+
+    if (!existingUser.emailVerified) {
+        const verificationToken = await generateVerificationToken(
+            existingUser.email
+        );
+
+        await sendVerificationEmail(
+            verificationToken.email,
+            verificationToken.token
+        );
+        return { success: "Confirmation email sent!" };
+    }
 
     try {
         await signIn("credentials", {
--- actions/register.ts
+++ actions/register.ts
@@ -1,31 +1,40 @@
 "use server";
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
     if (!validatedFields.success) return { error: "Invalid fields" };
 
-    const { email, password, confirmPassword, name} = validatedFields.data
-    if (password !== confirmPassword) return { error: "Passwords do not match"}
+    const { email, password, confirmPassword, name } = validatedFields.data;
+    if (password !== confirmPassword)
+        return { error: "Passwords do not match" };
 
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const existingUser = await getUserByEmail(email)
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const existingUser = await getUserByEmail(email);
 
-    if(existingUser) return {error: "Email already in use!"}
+    if (existingUser) return { error: "Email already in use!" };
 
     await db.user.create({
         data: {
             name,
             email,
-            password: hashedPassword
-        }
-    })
+            password: hashedPassword,
+        },
+    });
 
     //TODO: Send verification token email
 
-    return { success: "Register successfull" };
+    const verificationToken = await generateVerificationToken(email);
+    await sendVerificationEmail(
+        verificationToken.email,
+        verificationToken.token
+    );
+
+    return { success: "Confirmation email sent!" };
 };
