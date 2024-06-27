aca21d2a6335cfd472ace516e336e1c0cb309d8d
Jhon <eduardcamayo13@gmail.com>
2024-06-27 09:27:36 -0500

    Arreglando estilos

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -2,23 +2,23 @@ import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["600"],
+    subsets: ["latin"],
+    weight: ["600"],
 });
 
 interface HeaderProps {
-  label: string;
+    label: string;
 }
 
 const Header = ({ label }: HeaderProps) => {
-  return (
-    <div className="w-full flex flex-col gap-y-4 items-center justify-center">
-      <h1 className={cn("text-3xl font-semibold", poppins.className)}>
-        🔏 Auth
-      </h1>
-      <p className="text-muted-foreground text-sm">{label}</p>
-    </div>
-  );
+    return (
+        <div className="w-full flex flex-col gap-y-4 items-center justify-center">
+            <h1 className={cn("text-3xl font-semibold", poppins.className)}>
+                🔏 Auth
+            </h1>
+            <p className="text-muted-foreground text-sm">{label}</p>
+        </div>
+    );
 };
 
 export default Header;
