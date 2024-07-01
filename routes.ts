dc655af26ca4fed877f1086fa3c1ea0ad4ebeb72
Jhon <eduardcamayo13@gmail.com>
2024-06-30 22:43:56 -0500

    Agregando nueva ruta

--- routes.ts
+++ routes.ts
@@ -3,7 +3,7 @@
  @type {string[]}
 */
 
-export const publicRoutes = ["/"];
+export const publicRoutes = ["/", "/auth/new-verification"];
 
 /**
  * An array of routes that are used for authentication
