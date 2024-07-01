8a36d081c3c0ab0c324d411fc319dbd5989b12cc
Jhon <eduardcamayo13@gmail.com>
2024-07-01 13:13:50 -0500

    Agregando nuevas rutas

--- routes.ts
+++ routes.ts
@@ -11,7 +11,13 @@ export const publicRoutes = ["/", "/auth/new-verification"];
  * @type {string[]}
  */
 
-export const authRoutes = ["/auth/login", "/auth/register", "/auth/error"];
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register",
+    "/auth/error",
+    "/auth/reset",
+    "/auth/new-password",
+];
 
 /**
  * The prefix for API authentication routes
