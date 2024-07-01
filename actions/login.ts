98a2710c18980f6697ab09dc43fd7e9e363a7bb1
Jhon <eduardcamayo13@gmail.com>
2024-07-01 18:50:15 -0500

    Agregando 2FA

--- actions/login.ts
+++ actions/login.ts
@@ -5,16 +5,22 @@ import { LoginSchema } from "@/schemas";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
 import { getUserByEmail } from "@/data/user";
+import {
+    generateTwoFactorToken,
+    generateVerificationToken,
+} from "@/lib/tokens";
+import { sendTwoFactorTokenEmail, sendVerificationEmail } from "@/lib/mail";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 import { error } from "console";
-import { generateVerificationToken } from "@/lib/tokens";
-import { sendVerificationEmail } from "@/lib/mail";
+import { db } from "@/lib/db";
+import { getTwoFactorConfirmationByUserById } from "@/data/two-factor-confirmation";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
     if (!validatedFields.success) {
         return { error: "Invalid fields" };
     }
-    const { email, password } = validatedFields.data;
+    const { email, password, code } = validatedFields.data;
 
     const existingUser = await getUserByEmail(email);
     if (!existingUser || !existingUser.email || !existingUser.password) {
@@ -33,6 +39,54 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
         return { success: "Confirmation email sent!" };
     }
 
+    if (existingUser.isTwoFactorEnabled && existingUser.email) {
+        if (code) {
+            // todo: Verify code
+            const twoFactorToken = await getTwoFactorTokenByEmail(
+                existingUser.email
+            );
+            if (!twoFactorToken) {
+                return { error: "Invalid code!" };
+            }
+            if (twoFactorToken.token !== code) {
+                return { error: "Invalid code!" };
+            }
+            const hasExpired = new Date(twoFactorToken.expires) < new Date();
+
+            if (hasExpired) {
+                return { error: "Code expired!" };
+            }
+
+            await db.twoFactorToken.delete({
+                where: { id: twoFactorToken.id },
+            });
+
+            const existingConfirmation =
+                await getTwoFactorConfirmationByUserById(existingUser.id);
+
+            if (existingConfirmation) {
+                await db.twoFactorConfirmation.delete({
+                    where: { id: existingConfirmation.id },
+                });
+            }
+
+            await db.twoFactorConfirmation.create({
+                data: {
+                    userId: existingUser.id,
+                },
+            });
+        } else {
+            const twoFactorToken = await generateTwoFactorToken(
+                existingUser.email
+            );
+            await sendTwoFactorTokenEmail(
+                twoFactorToken.email,
+                twoFactorToken.token
+            );
+            return { twoFactor: true };
+        }
+    }
+
     try {
         await signIn("credentials", {
             email,
@@ -40,7 +94,6 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
             redirectTo: DEFAULT_LOGIN_REDIRECT,
         });
     } catch (error) {
-        console.log("erosejrioe: ", error);
         if (error instanceof AuthError) {
             switch (error.type) {
                 case "CredentialsSignin":
