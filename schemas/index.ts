c8eb01fc06ec005240693b7687290fbb04d0b5cf
Jhon <eduardcamayo13@gmail.com>
2024-07-01 13:14:39 -0500

    Agregando nuevos schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,19 +1,31 @@
-import * as z from "zod"
+import * as z from "zod";
 
 export const LoginSchema = z.object({
     email: z.string().email({
-        message: "Email is required"
+        message: "Email is required",
     }),
-    password: z.string().min(6,{
-        message: "Password is required"
-    })
-})
+    password: z.string().min(6, {
+        message: "Password is required",
+    }),
+});
+
+export const ResetSchema = z.object({
+    email: z.string().email({
+        message: "Email is required",
+    }),
+});
+
+export const NewPasswordSchema = z.object({
+    password: z.string().min(6, {
+        message: "Minimum of 6 characters required",
+    }),
+});
 
 export const RegisterSchema = z.object({
-    email: z.string().email({message: "Email is required"}),
-    password: z.string().min(6,{message: "Minimum 6 caracters required"}),
-    confirmPassword: z.string().min(6, {message: "Minimum 6 caracters required"}),
-    name: z.string().min(1,{message: "Name is required"})
-
-})
-
+    email: z.string().email({ message: "Email is required" }),
+    password: z.string().min(6, { message: "Minimum 6 caracters required" }),
+    confirmPassword: z
+        .string()
+        .min(6, { message: "Minimum 6 caracters required" }),
+    name: z.string().min(1, { message: "Name is required" }),
+});
