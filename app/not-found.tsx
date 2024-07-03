160d418ef11a6308c58266cec356491be80be0c9
Jhon <eduardcamayo13@gmail.com>
2024-07-02 22:49:08 -0500

    Agregando página not found

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import NotFoundForm from "@/components/not-found-form";
+
+const NotFound = () => {
+    return (
+        <div className="flex items-center justify-center h-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+            <NotFoundForm />
+        </div>
+    );
+};
+
+export default NotFound;
