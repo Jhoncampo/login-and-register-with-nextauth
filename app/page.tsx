6c4ca255dcc4df5c454c3df64d11219d2822e07a
Jhon <eduardcamayo13@gmail.com>
2024-07-02 22:49:38 -0500

    Arreglando formateo de texto

--- app/page.tsx
+++ app/page.tsx
@@ -4,31 +4,33 @@ import { cn } from "@/lib/utils";
 import LoginButton from "@/components/auth/login-button";
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["600"],
+    subsets: ["latin"],
+    weight: ["600"],
 });
 
 export default function Home() {
-  return (
-    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <div className="space-y-6">
-        <h1
-          className={cn(
-            "text-6xl font-semibold text-white drop-shadow-md",
-            poppins.className
-          )}
-        >
-          🔏 Auth
-        </h1>
-        <p className="text-white text-lg">A simple authentication service</p>
-        <div className="w-full flex items-center justify-center">
-          <LoginButton>
-            <Button className="" size="lg" variant="secondary">
-              Sing in
-            </Button>
-          </LoginButton>{" "}
-        </div>
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+            <div className="space-y-6">
+                <h1
+                    className={cn(
+                        "text-6xl font-semibold text-white drop-shadow-md",
+                        poppins.className
+                    )}
+                >
+                    🔏 Auth
+                </h1>
+                <p className="text-white text-lg">
+                    A simple authentication service
+                </p>
+                <div className="w-full flex items-center justify-center">
+                    <LoginButton>
+                        <Button className="" size="lg" variant="secondary">
+                            Sing in
+                        </Button>
+                    </LoginButton>{" "}
+                </div>
+            </div>
+        </main>
+    );
 }
