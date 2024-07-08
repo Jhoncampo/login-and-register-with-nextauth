a8e9723c194026030665008eb212c353f25c97ee
Jhon <eduardcamayo13@gmail.com>
2024-07-07 21:12:02 -0500

    Agregando pequeños cambios

--- app/(protected)/client/page.tsx
+++ app/(protected)/client/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import UserInfo from "@/components/user-info";
 import { useCurrentUser } from "@/hooks/use-current.user";
-import React from "react";
 
 const ClientPage = () => {
     const user = useCurrentUser();
