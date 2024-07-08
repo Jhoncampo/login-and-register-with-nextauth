4cbafd38c777bbc633073a2fcf4359c7fecff732
Jhon <eduardcamayo13@gmail.com>
2024-07-07 21:10:43 -0500

    Terminando página de admin

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+"use client";
+
+import { admin } from "@/actions/admin";
+import RoleGate from "@/components/auth/role-gate";
+import FormSuccess from "@/components/form-success";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { UserRole } from "@prisma/client";
+import { toast } from "sonner";
 
 const AdminPage = () => {
-  return (
-    <div>AdminPage</div>
-  )
-}
+    const onServerActionClick = () => {
+        admin().then((data) => {
+            if (data.error) {
+                toast.error(data.error);
+            }
+            if (data.success) {
+                toast.success(data.success);
+            }
+        });
+    };
+    const onApiRouteClick = () => {
+        fetch("/api/admin").then((response) => {
+            if (response.ok) {
+                toast.success("Allowed API Route!");
+            } else {
+                toast.error("Forbidden API Route");
+            }
+        });
+    };
+    return (
+        <Card className="w-full max-w-[600px]">
+            <CardHeader>
+                <p className="text-2xl font-semibold text-center">🔑 Admin</p>
+            </CardHeader>
+            <CardContent className="space-y-4">
+                <RoleGate allowedRole={UserRole.ADMIN}>
+                    <FormSuccess message="You are allowed to see this content!" />
+                </RoleGate>
+                <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+                    <p className="text-sm font-medium">Admin-only API Route</p>
+                    <Button onClick={onApiRouteClick}>Click to test</Button>
+                </div>
 
-export default AdminPage
+                <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+                    <p className="text-sm font-medium">
+                        Admin-only Server Action
+                    </p>
+                    <Button onClick={onServerActionClick}>Click to test</Button>
+                </div>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default AdminPage;
