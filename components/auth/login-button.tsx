dd93e2722e660b88ca387606e0324edf4756142f
Jhon <eduardcamayo13@gmail.com>
2024-07-10 12:12:51 -0500

    Agregando el modal

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import LoginForm from "@/components/auth/login-form";
 
 interface LoginButtonProps {
     children: React.ReactNode;
     mode?: "modal" | "redirect";
-    asChildren?: boolean;
+    asChild?: boolean;
     href: string;
 }
 
@@ -11,10 +13,17 @@ const LoginButton = ({
     children,
     href,
     mode = "redirect",
-    asChildren,
+    asChild,
 }: LoginButtonProps) => {
     if (mode === "modal") {
-        return <span>Todo: Implement modal</span>;
+        return (
+            <Dialog>
+                <DialogTrigger asChild={asChild}>{children}</DialogTrigger>
+                <DialogContent className="p-0 w-auto bg-transparent border-none">
+                    <LoginForm />
+                </DialogContent>
+            </Dialog>
+        );
     }
 
     return (
