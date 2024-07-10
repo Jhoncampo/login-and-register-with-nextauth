68ab3ed9c57e44d35f25ad3926fd97853cf8ffa7
Jhon <eduardcamayo13@gmail.com>
2024-07-09 22:28:42 -0500

    Agregando schema de settings

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,4 @@
+import { UserRole } from "@prisma/client";
 import * as z from "zod";
 
 export const LoginSchema = z.object({
@@ -7,7 +8,7 @@ export const LoginSchema = z.object({
     password: z.string().min(6, {
         message: "Password is required",
     }),
-    code: z.optional(z.string())
+    code: z.optional(z.string()),
 });
 
 export const ResetSchema = z.object({
@@ -21,6 +22,38 @@ export const NewPasswordSchema = z.object({
         message: "Minimum of 6 characters required",
     }),
 });
+
+export const SettingsSchema = z
+    .object({
+        name: z.optional(z.string()),
+        isTwoFactorEnabled: z.optional(z.boolean()),
+        role: z.enum([UserRole.ADMIN, UserRole.USER]),
+        email: z.optional(z.string().email()),
+        password: z.optional(z.string().min(6)),
+        newPassword: z.optional(z.string().min(6)),
+    })
+    .refine(
+        (data) => {
+            if (data.password && !data.newPassword) {
+                return false;
+            }
+
+            return true;
+        },
+        { message: "New password is required!", path: ["newPassword"] }
+    )
+    .refine(
+        (data) => {
+            if (data.newPassword && !data.password) {
+                return false;
+            }
+            return true;
+        },
+        {
+            message: "Password is required",
+            path: ["password"],
+        }
+    );
 
 export const RegisterSchema = z.object({
     email: z.string().email({ message: "Email is required" }),
