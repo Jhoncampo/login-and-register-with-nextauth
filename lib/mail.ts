337fab54750be44d1d6702c809dd1f1e3e77b178
Jhon <eduardcamayo13@gmail.com>
2024-07-10 12:50:06 -0500

    Agregando dominio para los emails

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,7 +5,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "mail@empresataxis.online",
         to: email,
         subject: "2FA Code",
         html: `<p>Your 2FA code: ${token}</p>`,
@@ -15,7 +15,7 @@ export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
     const resetLink = `${domain}/auth/new-password?token=${token}`;
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "mail@empresataxis.online",
         to: `${email}`,
         subject: "Reset your password",
         html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
@@ -26,7 +26,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "mail@empresataxis.online",
         to: `${email}`,
         subject: "Confirm your email",
         html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
